375f600594bc82636a719b0ee26c3bda613df210
Dave Shepherd <dave.shepherd@endor.me.uk>
2024-04-14 13:41:13 +0100

    chore(package.json): tidy up project configuration

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,20 +1,17 @@
 import { awscdk, github } from 'projen';
+
 const project = new awscdk.AwsCdkTypeScriptApp({
   cdkVersion: '2.1.0',
   copyrightOwner: 'Dave Shepherd',
   defaultReleaseBranch: 'main',
+  description:
+    'A scraper and APIs for getting river readings from the Environment Agency',
+  githubOptions: {
+    projenCredentials: github.GithubCredentials.fromApp({}),
+  },
   license: 'MIT',
   name: 'river-levels',
   projenrcTs: true,
   release: true,
-
-  githubOptions: {
-    projenCredentials: github.GithubCredentials.fromApp({}),
-  },
-
-  // deps: [],                /* Runtime dependencies of this module. */
-  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
-  // devDeps: [],             /* Build dependencies for this module. */
-  // packageName: undefined,  /* The "name" in package.json. */
 });
 project.synth();
