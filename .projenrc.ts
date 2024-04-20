4d49ebfaa7b4024df746355fa7e93bdc9432292a
Dave Shepherd <dave.shepherd@endor.me.uk>
2024-04-20 14:32:22 +0100

    feature(workflow): add example new workflow to project

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,4 +1,6 @@
 import { awscdk, github } from 'projen';
+import { GithubWorkflow } from 'projen/lib/github';
+import { JobPermission } from 'projen/lib/github/workflows-model';
 
 const project = new awscdk.AwsCdkTypeScriptApp({
   cdkVersion: '2.1.0',
@@ -14,4 +16,39 @@ const project = new awscdk.AwsCdkTypeScriptApp({
   projenrcTs: true,
   release: true,
 });
+
+new GithubWorkflow(project.github!, 'Run-Tests').addJob('build', {
+  runsOn: ['ubuntu-latest'],
+  permissions: {
+    pullRequests: JobPermission.WRITE,
+  },
+  steps: [
+    {
+      name: 'Check-out-code',
+      uses: 'actions/checkout@v3',
+    },
+    {
+      name: 'Set-up-node',
+      uses: 'actions/setup-node@v1',
+    },
+    {
+      name: 'Install dependencies',
+      run: 'echo "npm install"',
+    },
+  ],
+});
+
+project.github!.workflows[3].on({
+  pullRequestTarget: {
+    types: [
+      'opened',
+      'synchronize',
+      'reopened',
+      'labeled',
+      'ready_for_review',
+      'edited',
+    ],
+  },
+});
+
 project.synth();
