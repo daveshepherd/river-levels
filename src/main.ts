3edcf472197e2bc85ee5c831274337e3a121a57c
Dave Shepherd <dave.shepherd@endor.me.uk>
2024-06-01 13:32:12 +0100

    feat(storage-stack): :sparkles: deploy to mutliple regions

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,12 @@ import { StorageStack } from './storage-stack';
 
 const app = new App();
 
-const storageStack = new StorageStack(app, 'RiverLevels-dev', {});
+const storageStack = new StorageStack(app, 'RiverLevels-dev', {
+  env: {
+    region: 'eu-west-2',
+  },
+  replicaRegions: ['eu-west-1'],
+});
 Tags.of(storageStack).add('endor:ManagedBy', 'cdk');
 Tags.of(storageStack).add('endor:Stage', 'development');
 
